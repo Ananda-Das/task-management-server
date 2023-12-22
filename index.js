00e11356d54ee713f9a6fa2731ab9a10f16b4cc4
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-12-22 18:00:48 +0600

    Basic Setup

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:5000','http://localhost:5176'],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 204,
+    exposedHeaders: ['Access-Control-Allow-Headers'],
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qfsxze0.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // users, admin and moderator related
+    const usersCollection = client.db('taskManagerDB').collection('users');
+    const taskcollection = client.db('taskManagerDB').collection('task');
+
+    app.get('/addtask', async (req, res) => {
+      const result = await taskcollection.find().toArray();
+      res.send(result);
+    });
+
+
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Tech On Fire');
+});
+
+app.listen(port, () => {
+  console.log(`Port is running on: ${port}`);
+});
