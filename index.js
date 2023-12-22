20c4f9d26e379ebd66587eb374bd985a7589320e
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-12-22 18:01:47 +0600

    Add task functionality Added

--- index.js
+++ index.js
@@ -45,7 +45,62 @@ async function run() {
     });
 
 
-    
+    app.post('/addtask', async (req, res) => {
+      const taskadd = req.body;
+      const data = {
+        title: taskadd.title,
+        description: taskadd.description,
+        date: taskadd.date,
+        priority: taskadd.priority,
+        status: 'todo',
+      };
+      const result = await taskcollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.patch('/status', async (req, res) => {
+      const id = req.query.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await taskcollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete('/delete', async (req, res) => {
+      const id = req.query.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await taskcollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.put('/update', async (req, res) => {
+      const id = req.query.id;
+      const filter = { _id: new ObjectId(id) };
+      const data = req.body;
+      const updatedDoc = {
+        $set: {
+          title: data.title,
+          description: data.description,
+          priority: data.priority,
+          deadline: data.deadline,
+          email: data.email,
+        },
+      };
+      const result = await taskcollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.post('/task', async (req, res) => {
+      const data = req.body;
+      const result = await taskcollection.insertOne(data);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
